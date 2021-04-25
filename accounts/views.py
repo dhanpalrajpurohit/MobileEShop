5a7f7c2c4fa84f1872f7af5d0e9f0fc40d37bc21
Rajpurohit Dhanpal <rajpurohitdhanpal@gmail.com>
2021-04-25 13:37:19 +0530

    updated

--- accounts/views.py
+++ accounts/views.py
@@ -12,7 +12,7 @@ from django.contrib.auth.decorators import login_required
 
 @login_required
 def home(request):
-    return render(request , 'home.html')
+    return render(request , 'index.html')
 
 
 
--- bookEcommerece/settings.py
+++ bookEcommerece/settings.py
@@ -119,3 +119,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+############################################### E-Mail Setup for sending email 
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'mail@gmail.com'
+EMAIL_HOST_PASSWORD = 'password'
